9e6cfcf1370de89c6ea792a9237c8208c49d31cd
munz11 <47572559+munz11@users.noreply.github.com>
2020-03-08 17:27:54 +0100

    Created the TypeAST file
    The AST types specifies the types that the parser will use to generate the AST.

---
+++ TypesAST.fs
@@ -0,0 +1,44 @@
+// This file implements a module where we define a data type "expr"
+// to store represent arithmetic expressions
+module TypesAST
+type VarX = string
+type AExp =
+  | Num of float
+  | TimesExpr of (AExp * AExp)
+  | DivExpr of (AExp * AExp)
+  | PlusExpr of (AExp * AExp)
+  | MinusExpr of (AExp * AExp)
+  | PowExpr of (AExp * AExp)
+  | UPlusExpr of (AExp)
+  | UMinusExpr of (AExp)
+  | BracExpr of (AExp)
+  | Xval of (VarX)
+  | ArrayExpr of (string * AExp)
+
+
+type BExp =
+| BitAndB of BExp * BExp
+| BitOrB of BExp * BExp
+| LogAndB of BExp * BExp
+| LogOrB of BExp * BExp
+| LogNotB of BExp
+| BEqual of AExp * AExp
+| NotEqualB of AExp * AExp
+| GThanB of AExp * AExp
+| LThanB of AExp * AExp
+| GEThanB of AExp * AExp
+| LEThanB of AExp * AExp
+| BracB of BExp
+| Wut of string // either true or false
+
+
+type CExp =
+    |AssignC of (VarX * AExp)
+    |AssignArrayC of (VarX * AExp * AExp)
+    |SkipC
+    |StateC of (CExp *CExp)
+    |IfStateC of (GCExp)
+    |DoloopC of (GCExp)
+and GCExp =
+    | ARROWGC of (BExp * CExp)
+    | StateGC of (GCExp *GCExp)
